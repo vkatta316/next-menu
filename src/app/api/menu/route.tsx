53b09d91c55a9c650d707b69eb6cce7866fb2401
vinay.katta <vinay.katta@servicenow.com>
2023-11-23 21:41:08 -0600

    Apply tailwind css for the Menu Page

--- src/app/api/menu/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import menu from "@/data/menu.js";
-import { delay } from "@/utils";
-
-export async function GET() {
-await delay(2000);
-  return NextResponse.json({data: menu});
-}
